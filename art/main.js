4dfa39ceea27c4135a5a7bf1fdb5be50138b69bf
ben <yipjones2@gmail.com>
2023-10-10 18:39:00 +0100

    okay mostly fine, except background of modal inst animating, might try moving transition to js, or making it change class for the change

--- art/main.js
+++ art/main.js
@@ -1,8 +1,10 @@
 function ImageClicked(image){
 	modal = document.getElementById("modal");
-	modalImage = document.getElementById("modal-image")
-	modalHeader = document.getElementById("modal-text-header")
+	modalImage = document.getElementById("modal-image");
+	modalHeader = document.getElementById("modal-text-header");
 	modal.style.display = "block";
+	modal.style.backdropFilter = "blur(5px) saturate(40%)";
+	modal.style.backgroundColor = "rgba(0,0,0,0.9)"
 	modalImage.src = image.src;
 	modalHeader.innerHTML = image.getAttribute("data-name");
 	console.log("success")
